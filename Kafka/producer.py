6ef892f3ef818984574252b8e25c7e9e903f3471
Forest Pfeiffer <forest.pfeiffer@gmail.com>
2019-06-16 15:32:50 -0700

    Add Data directory with Data dictionary
    Add Data directory with Data dictionary and remove outdated jupyter notebooks

--- Kafka/producer.py
+++ Kafka/producer.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 ############################################################
 # Author: Forest Pfeiffer
+#Based on producer.py written by Ryan Hebel during Insight DE
+#2018C session
 
 #Create a Kafka producer
 # that reads data from a S3 bucket and sends it to a topic.
@@ -25,21 +21,27 @@ def main():
 
         #create Kafka producer that communicates with master node of ec2 instance running Kafka
         producer = KafkaProducer(bootstrap_servers = 'ec2-50-112-13-159.us-west-2.compute.amazonaws.com:9092')
-        
-        #read credentials from dwg.cfg file (note this file will not be stored on github for security reasons)
+
+        #read credentials from dwg.cfg file (note this file will not be stored
+        #on github for security reasons)
         config = configparser.ConfigParser()
         config.read('dwh.cfg')
         AWS_ACCESS_KEY_ID = config.get('AWS', 'KEY')
         AWS_SECRET_ACCESS_KEY = config.get('AWS', 'SECRET')
-        
+
         #creates bucket that points to data
         s3 = boto3.resource('s3', aws_access_key_id = AWS_ACCESS_KEY_ID, aws_secret_access_key = AWS_SECRET_ACCESS_KEY)
         bucket = s3.Bucket('deutsche-boerse-xetra-pds')
-        
-        #the deutsche-boerse-xetra-pds bucket contains a list of csv links that point to data for each hour of trading
+
+        #the deutsche-boerse-xetra-pds bucket contains a list of csv links that
+        #point to data for each hour of trading
         #first, iterate through each object (link to csv) in the bucket
         for object in bucket.objects.all():
                 #filter for non-trading hours (empty csv) by size
+                print(object)
+                break
+                #files with size 136 bytes indicate off hours logging and
+                #should be ommitted
                 if object.size > 136:
                         url = 'https://s3.eu-central-1.amazonaws.com/deutsche-boerse-xetra-pds/' + object.key
                         data = pd.read_csv(url)
@@ -50,15 +52,8 @@ def main():
                                         output = output + str(element) + "^"
                                 producer.send('rawDBGData', output.encode())
                                 producer.flush()
-        producer.close()
+        #producer.close()
         return
 
 if __name__ == '__main__':
         main()
-
-
-# In[ ]:
-
-
-
-
