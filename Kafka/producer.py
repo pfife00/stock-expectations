2744d007d5df37825ad10455162a0f4cc53e03a6
Forest Pfeiffer <forest.pfeiffer@gmail.com>
2019-06-16 12:07:01 -0700

    Upload producer.py script
    Created producer.py script to connect to Xetra dataset in S3. Removed Untitled1.ipynb

---
+++ Kafka/producer.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+############################################################
+# Author: Forest Pfeiffer
+
+#Create a Kafka producer
+# that reads data from a S3 bucket and sends it to a topic.
+############################################################
+
+#to install boto3, run the following command:
+#pip install boto3
+
+from kafka import KafkaProducer
+import time
+import boto3
+import botocore
+import pandas as pd
+import configparser
+
+def main():
+
+        #create Kafka producer that communicates with master node of ec2 instance running Kafka
+        producer = KafkaProducer(bootstrap_servers = 'ec2-50-112-13-159.us-west-2.compute.amazonaws.com:9092')
+        
+        #read credentials from dwg.cfg file (note this file will not be stored on github for security reasons)
+        config = configparser.ConfigParser()
+        config.read('dwh.cfg')
+        AWS_ACCESS_KEY_ID = config.get('AWS', 'KEY')
+        AWS_SECRET_ACCESS_KEY = config.get('AWS', 'SECRET')
+        
+        #creates bucket that points to data
+        s3 = boto3.resource('s3', aws_access_key_id = AWS_ACCESS_KEY_ID, aws_secret_access_key = AWS_SECRET_ACCESS_KEY)
+        bucket = s3.Bucket('deutsche-boerse-xetra-pds')
+        
+        #the deutsche-boerse-xetra-pds bucket contains a list of csv links that point to data for each hour of trading
+        #first, iterate through each object (link to csv) in the bucket
+        for object in bucket.objects.all():
+                #filter for non-trading hours (empty csv) by size
+                if object.size > 136:
+                        url = 'https://s3.eu-central-1.amazonaws.com/deutsche-boerse-xetra-pds/' + object.key
+                        data = pd.read_csv(url)
+                        #read through each line of csv and send the line to the kafka topic
+                        for index, row in data.iterrows():
+                                output = ''
+                                for element in row:
+                                        output = output + str(element) + "^"
+                                producer.send('rawDBGData', output.encode())
+                                producer.flush()
+        producer.close()
+        return
+
+if __name__ == '__main__':
+        main()
+
+
+# In[ ]:
+
+
+
+
