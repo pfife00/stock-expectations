18ff3d2415d2212812f8d456d1697480fe0e66e5
Forest Pfeiffer <forest.pfeiffer@gmail.com>
2019-06-23 13:44:04 -0700

    Update spark preprocessing.py file
    Update spark preprocessing.py file

--- Spark/preprocessing.py
+++ Spark/preprocessing.py
@@ -1,48 +1,51 @@
-#!/usr/bin/env python
-import os
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 pyspark-shell'
+#!/usr/bin/env python3#
+#
+
+from __future__ import print_function
+import sys
+from pyspark.streaming.kafka import KafkaUtils, TopicAndPartition
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils, TopicAndPartition
-from pyspark.sql import SQLContext
-#import psycopg2
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import json
+from pyspark.sql import SparkSession
+
+
+
 
 def main():
-    #Create spark job and name it
-    sc = SparkContext(appName = 'updated_testjob')
-    #Prevent spark from displaying all logs messages
-    sc.setLogLevel('Error')
-    #Provides methods used to create Dstreams from various input source
-    #Note: metadata.broker.list might be depreciated and need to be replaced
-    #with bootstrap.servers
-    ssc = StreamingContext(sc, 1)
+    """
+    Apply ETL on Spark Stream
+    """
+    batch_duration = 5
+    spark_session = SparkSession.builder.appName("stocks_monitoring").getOrCreate()
+    sc = spark_session.sparkContext
+    sc.setLogLevel("ERROR")
+    ssc = StreamingContext(sc, batch_duration)
+    #sqlContext = SQLContext(sc)
 
-    #set kafka offsets
     topic = 'rawDBGData'
     partition = 0
     start = 0
     topicpartition = TopicAndPartition(topic, partition)
     fromoffset = {topicpartition: int(start)}
     #parse the row into separate components
-    #kafkaStream = KafkaUtils.createDirectStream(ssc,
-    #                ['rawDBGData'], {'metadata.broker.list':
-    #                'ec2-50-112-13-159.us-west-2.compute.amazonaws.com:9092'})
+    kafka_ips = '10.0.0.11:9092, 10.0.0.9:9092, 10.0.0.6:9092'
     kafkaStream = KafkaUtils.createDirectStream(ssc,
-                    ['rawDBGData'], {'metadata.broker.list':
-                    '10.0.0.11:9092, 10.0.0.9:9092, 10.0.0.4:9092'}, fromOffsets = fromoffset)
+                    [topic], {'metadata.broker.list':
+                    kafka_ips}, fromOffsets = fromoffset)
+
+
+    #kafkaStream = KafkaUtils.createDirectStream(ssc,
+    #                [topic], {'metadata.broker.list':
+    #                kafka_ips})
 
     #parse the row into separate components
-    filteredStream = kafkaStream.map(lambda line: line[1].split("^"))
-    filteredStream.pprint()
-    #identify the stocks that we would like to buy (those with increasing price) and calculate number of shares to purchase
-    buy = filteredStream.filter(lambda line: (float(line[11]) - float(line[8])) > 0.0).map(lambda line: [line[1], line[6], line[7], line[8], int(1000*(float(line[11]) - float(line[8]))/float(line[8]))]).filter(lambda line: line[4] > 0)
-    #buy.pprint()
-    #Set entry point for working with structured data (rows and columns)
-    sqlContext = SQLContext(sc)
-
+    kafkaStream = kafkaStream.window(5)
+    filteredStream = kafkaStream.flatMap(lambda line: line[1].split("^")).pprint()
     ssc.start()
     ssc.awaitTermination()
 
-
-if __name__ == '__main__':
-        main()
+if __name__ == "__main__":
+    main()
